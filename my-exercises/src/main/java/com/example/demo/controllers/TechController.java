1e9bf5c80e5574caff6c8f5fe12fa676b8f9384c
vic-fmr <vfm@poli.br>
2025-02-26 01:38:18 -0300

    basic auth config

--- my-exercises/src/main/java/com/example/demo/controllers/TechController.java
+++ my-exercises/src/main/java/com/example/demo/controllers/TechController.java
@@ -4,6 +4,7 @@ import com.example.demo.models.TechModel;
 import com.example.demo.services.TechService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class TechController {
 
     @Autowired
     private TechService techService;
+
 
     @PostMapping(value = "/create")
     public ResponseEntity<TechModel> createTech(@RequestBody TechModel techModel) {
